b6e7a4120052c82f2768ed1897bcb09d57fcc65f
JonathanSKent <kent.jonathan.s@gmail.com>
2019-04-26 22:13:24 -0500

    Got some more work done

--- Settings.py
+++ Settings.py
@@ -6,6 +6,19 @@ Created on Fri Apr 26 19:19:10 2019
 @author: jonathan
 """
 
+import torch
+
+# Prints these to demarcate between actions
+demarc_long = "~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~"
+def d1():
+    print(demarc_long)
+    
+demarc_short = "~#~#~#~#~#~"
+def d2():
+    print(demarc_short)
+
+# Whether or not a gpu is available
+device = ['cpu', 'cuda'][torch.cuda.is_available()]
+
 # Tickers of the stocks being traded
 stock_ticks = ['f', 'goog', 'cost', 'hon', 'amd']
-
--- StockData.py
+++ StockData.py
@@ -7,13 +7,31 @@ Created on Fri Apr 26 19:11:02 2019
 """
 
 from yahoo_fin import stock_info
+import torch
+import numpy as np
 
 import Settings
 
 # Object that will fetch live stock price data
-class data_fetch:
+class data_obj:
     def __init__(self, st = Settings.stock_ticks):
+        print("Initializing data object...")
         self.stocks = st
+        self.curr_price = get_data(self.stocks)
+        self.basis = torch.tensor(self.curr_price).to(Settings.device)
+        self.pct_hist = torch.ones(1, len(self.stocks))
+        self.curr_pct = self.pct_hist[0]
+        print("Data object initialized. Price basis: ", list(np.array(self.basis.cpu())))
+        Settings.d1()
         
-    def get_data(self):
-        return({tick : stock_info.get_live_price(tick) for tick in self.stocks})
+    def update(self):
+        print("Updating stock price information...")
+        self.curr_price = get_data(self.stocks)
+        self.curr_pct = torch.tensor(self.curr_price).to(Settings.device) / self.basis
+        self.pct_hist = torch.cat((self.pct_hist, self.curr_pct.reshape([1, -1]).cpu()))
+        print("Stock price information updated.")
+        Settings.d2()
+        
+    
+def get_data(stocks):
+    return([stock_info.get_live_price(tick) for tick in stocks])
