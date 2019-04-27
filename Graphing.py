91fc3932149d635c0848e21e70572831e1827509
JonathanSKent <kent.jonathan.s@gmail.com>
2019-04-26 22:56:39 -0500

    Got some more work done

--- Graphing.py
+++ Graphing.py
@@ -6,7 +6,7 @@ Created on Fri Apr 26 22:25:13 2019
 @author: Jonathan S. Kent
 """
 
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import numpy as np
 
 import Settings
@@ -15,6 +15,9 @@ import StockData
 # Graphs the percent changes of stock price multipliers over a day
 def stock_multiplier_graph(multipliers):
     for i in multipliers:
-        matplotlib.pyplot.plot(i)
-    matplotlib.pyplot.figure()
+        plt.plot(i)
+    axes = plt.gca()
+    axes.set_xlim([-5, Settings.minutes + 5])
+    plt.xticks(30 * np.arange(14), Settings.xlabels, rotation = Settings.rot)
+    plt.figure()
     
--- Settings.py
+++ Settings.py
@@ -22,3 +22,15 @@ device = ['cpu', 'cuda'][torch.cuda.is_available()]
 
 # Tickers of the stocks being traded
 stock_ticks = ['f', 'goog', 'cost', 'hon', 'amd']
+
+# Minutes in a trading day, from 9:30 AM EST to 4:00 PM EST
+minutes = 390
+
+# X-axis tick labels on the multiplier graph
+xlabels = ['9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', 
+          '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', 
+          '1:30 PM', '2:00 PM', '2:30 PM', '3:90 PM', 
+          '3:30 PM', '4:00 PM']
+
+# Rotation on the X-axis labels
+rot = -60
