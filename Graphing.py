b4d106b2cc0d47e00b596889969131319ee9216d
JonathanSKent <kent.jonathan.s@gmail.com>
2019-04-26 23:26:28 -0500

    Worked on graphs

--- Graphing.py
+++ Graphing.py
@@ -13,11 +13,16 @@ import Settings
 import StockData
 
 # Graphs the percent changes of stock price multipliers over a day
-def stock_multiplier_graph(multipliers):
-    for i in multipliers:
-        plt.plot(i)
+def stock_multiplier_graph(multipliers, fund_value):
+    for i in range(len(multipliers)):
+        plt.plot(multipliers[i], label = Settings.stock_ticks[i].upper(), linestyle = '--')
+    plt.plot(fund_value, label = 'Fund')
+    plt.xlabel("Time (EST)", fontsize = 'x-large')
+    plt.ylabel("Value Relative to Day Start", fontsize = 'x-large')
+    plt.title("Value of Assets Relative to Day Start", fontsize = 'xx-large')
     axes = plt.gca()
     axes.set_xlim([-5, Settings.minutes + 5])
-    plt.xticks(30 * np.arange(14), Settings.xlabels, rotation = Settings.rot)
+    plt.xticks(30 * np.arange(14), Settings.xlabels, rotation = -60)
+    plt.legend(loc='upper right')
     plt.figure()
     
--- Settings.py
+++ Settings.py
@@ -31,6 +31,3 @@ xlabels = ['9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM',
           '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', 
           '1:30 PM', '2:00 PM', '2:30 PM', '3:90 PM', 
           '3:30 PM', '4:00 PM']
-
-# Rotation on the X-axis labels
-rot = -60
